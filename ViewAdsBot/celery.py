b7cc843489c35535a45da85dc9f005b7d142439b
Mohamad Mohamadi <mohamadmohamadi249@gmail.com>
2023-06-10 16:27:48 +0000

    discover tasks

--- ViewAdsBot/celery.py
+++ ViewAdsBot/celery.py
@@ -9,5 +9,5 @@ app = Celery('ViewAdsBot')
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings', namespace="CELERY")
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks(lambda: [n.name for n in apps.get_app_configs()])
 app.conf.timezone = 'Asia/Tehran'
--- website/views.py
+++ website/views.py
@@ -1,5 +1,3 @@
 from django.shortcuts import render
-from viewAdsBot.tasks import get_orders
 
-get_orders()
 # Create your views here.

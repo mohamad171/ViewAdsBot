74f054f3a8025327a71ae6913745ac327e25cfd4
Mohamad Mohamadi <mohamadmohamadi249@gmail.com>
2023-06-11 10:46:48 +0330

    celery

--- ViewAdsBot/settings.py
+++ ViewAdsBot/settings.py
@@ -133,7 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://redis:6379/0'
-CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
 
 CELERY_TIMEZONE = 'Asia/Tehran'

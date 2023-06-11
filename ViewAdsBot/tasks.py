3e4b51174e5dcac6f2900b0ffb88e5b71c6a53ce
Mohamad Mohamadi <mohamadmohamadi249@gmail.com>
2023-06-11 11:13:14 +0330

    celery

--- ViewAdsBot/tasks.py
+++ ViewAdsBot/tasks.py
@@ -16,48 +16,50 @@ def get_orders(self):
 @celery_app.task(bind=True)
 def run_orders(self):
     orders = Order.objects.filter(status=Order.OrderStatusChoices.WATING,accept_to_start=True,start_at__lte=timezone.now())
-    join_orders = orders.filter(order_type=Order.OrderTypeChoices.JOIN).order_by("-count")
-    view_orders = orders.filter(order_type=Order.OrderTypeChoices.VIEW).order_by("-count")
-    accounts = []
+    if orders.count() > 0:
+        join_orders = orders.filter(order_type=Order.OrderTypeChoices.JOIN).order_by("-count")
+        view_orders = orders.filter(order_type=Order.OrderTypeChoices.VIEW).order_by("-count")
+        accounts = []
+        biggest_order = 0
+        if join_orders.count() > 0:
+            biggest_order = join_orders.first().count
+        if view_orders:
+            if view_orders.first().count > biggest_order:
+                biggest_order = view_orders.first()
 
-    biggest_order = join_orders.first().count
 
-    if view_orders.first().count > biggest_order:
-        biggest_order = view_orders.first()
-    
+        all_accounts = Account.objects.filter(is_active=True,is_logged_in=True).order_by("last_used")
+        if all_accounts.count() >= biggest_order:
 
-    all_accounts = Account.objects.filter(is_active=True,is_logged_in=True).order_by("last_used")
-    if all_accounts.count() >= biggest_order:
-
-        for a in all_accounts:
-            accounts.append({
-                "account":a,
-                "actions": []
-            })
-
-        for join_order in join_orders:
-            for account in accounts[0:join_order.count]:
-                account["actions"].append({
-                    "order_id":join_order.id,
-                    "order_type":join_order.order_type,
-                    "link":join_order.link
+            for a in all_accounts:
+                accounts.append({
+                    "account":a,
+                    "actions": []
                 })
-                join_order.status = Order.OrderStatusChoices.RUNNING
-                join_order.save()
-        
-        for view_order in view_orders:
-            for account in accounts[0:view_order.count]:
-                account["actions"].append({
-                    "order_id":view_order.id,
-                    "order_type":view_order.order_type,
-                    "link":view_order.link
-                })
-                view_order.status = Order.OrderStatusChoices.RUNNING
-                view_order.save()
-        
-    else:
-        pass
-    print(accounts)
+
+            for join_order in join_orders:
+                for account in accounts[0:join_order.count]:
+                    account["actions"].append({
+                        "order_id":join_order.id,
+                        "order_type":join_order.order_type,
+                        "link":join_order.link
+                    })
+                    join_order.status = Order.OrderStatusChoices.RUNNING
+                    join_order.save()
+
+            for view_order in view_orders:
+                for account in accounts[0:view_order.count]:
+                    account["actions"].append({
+                        "order_id":view_order.id,
+                        "order_type":view_order.order_type,
+                        "link":view_order.link
+                    })
+                    view_order.status = Order.OrderStatusChoices.RUNNING
+                    view_order.save()
+
+        else:
+            pass
+        print(accounts)
     # for account in accounts:
     #     print(account)
     #     results = do_action(account_data=account)
